3a2612c6def667e9f956a5b4e0b09df5ada155d1
pavneet singh <tallent71@netsetsoftware.com>
2022-12-08 14:25:23 +0530

    dynamic toast1

--- mktoast/src/main/java/com/mktoast/MkToast.kt
+++ mktoast/src/main/java/com/mktoast/MkToast.kt
@@ -3,7 +3,7 @@ package com.mktoast
 import android.content.Context
 import android.widget.Toast
 
-class MkToast(val mContext: Context) {
+open class MkToast(val mContext: Context) {
 
     fun messagePrint(msg:String){
         Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show()
